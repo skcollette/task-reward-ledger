935038b638ed3bd67c22cf1020d02969b46fdf44
skcollette <skcollette@gmail.com>
2026-08-05 11:55:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = "task-reward-ledger-v1";
 
+const ASSETS = [
+  "/task-reward-ledger/",
+  "/task-reward-ledger/index.html",
+  "/task-reward-ledger/manifest.webmanifest",
+  "/task-reward-ledger/icons/icon-192.png",
+  "/task-reward-ledger/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
